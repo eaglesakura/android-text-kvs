0043e2cf4b5268836766dde95dce4522db89938e
eaglesakura <eagle.sakura@gmail.com>
2016-03-15 22:27:23 +0900

    一時的にファイル名変更

--- src/test/java/com/eaglesakura/android/db/Text_KeyValueStoreTest.java
+++ src/test/java/com/eaglesakura/android/db/Text_KeyValueStoreTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
-public class TextKeyValueStoreTest extends UnitTestCase {
+public class Text_KeyValueStoreTest extends UnitTestCase {
 
     int mDbNum = 0;
 
